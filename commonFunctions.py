3cd6303cfdba4ca39dde06286016571aca374aaf
Koen J.A. Martens <32260034+kjamartens@users.noreply.github.com>
2021-09-21 10:33:48 +0200

    Update commonFunctions.py

--- commonFunctions.py
+++ commonFunctions.py
@@ -8,6 +8,7 @@
 # Output will always be an array with columns x - y - frame - intensity
 # Input can be ThunderSTORM file (readCSV) or rapidSTORM file (readTXT)
 def readCSV(filename):
+  import pandas
   #Determine which fields we want to read
   fields = ['x [nm]', 'y [nm]','frame', 'intensity [photon]']
 
@@ -18,6 +19,7 @@ def readCSV(filename):
   return data
 
 def readTXT(filename):
+  import pandas
   #Read the TXT as a CSV file, with tab-separated data, using only columns 1-4
   data = pandas.read_csv(filename, header=None, skiprows=1, sep=" ", usecols=[0,1,2,3])
   #Convert the data to a regular array
